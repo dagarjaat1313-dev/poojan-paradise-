be1896b8157e12b5c5904354bad5adfad03a02a5
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-26 03:50:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,23 @@
-import { Link } from 'react-router-dom';
-import { useCart } from '../context/CartContext';
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const { cartItems } = useCart();
-  const cartCount = cartItems.length;
-
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-brand">
-          <span className="brand-icon">🪔</span>
-          <span className="brand-text">Poojan Paradise</span>
+    <header className="navbar">
+      <div className="container">
+        <Link to="/" className="logo">
+          🛕 <span>Poojan Paradise</span>
         </Link>
 
-        <ul className="navbar-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/cart">Cart</Link>
+          <a href="#contact">Contact</a>
+        </nav>
 
-        <div className="navbar-actions">
-          <button className="search-btn" title="Search">
-            <span>🔍</span>
-          </button>
-          <Link to="/cart" className="cart-btn">
-            <span>🛒</span>
-            {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
-          </Link>
-          <button className="user-btn" title="Account">
-            <span>👤</span>
-          </button>
-        </div>
+        <button className="shop-btn">Shop Now</button>
       </div>
-    </nav>
+    </header>
   );
 }
