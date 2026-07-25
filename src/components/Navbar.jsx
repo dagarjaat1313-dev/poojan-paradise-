a8136738de79e2b2864e0e87b9af6eba3e67f54f
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-26 00:03:20 +0530

    Create components/Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar(){
+return(
+<nav className="navbar">
+<div className="container">
+<h2>🪔 Poojan Paradise</h2>
+
+<ul>
+<li>Home</li>
+<li>Shop</li>
+<li>About</li>
+<li>Contact</li>
+</ul>
+</div>
+</nav>
+)
+}
