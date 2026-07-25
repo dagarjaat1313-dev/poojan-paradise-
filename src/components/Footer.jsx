ae9c220c58a8d56e64f5539479aca16af00e6a9c
dagarjaat1313-dev <dagarjaat1313@gmail.com>
2026-07-26 02:32:14 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer(){
+  return(
+    <footer className="footer">
+
+      <h2>🪔 Poojan Paradise</h2>
+
+      <p>
+        Pure & Premium Pooja Samagri Delivered To Your Doorstep
+      </p>
+
+      <div className="footer-links">
+        <span>Home</span>
+        <span>Shop</span>
+        <span>About Us</span>
+        <span>Contact</span>
+      </div>
+
+      <p className="copyright">
+        © 2026 Poojan Paradise. All Rights Reserved.
+      </p>
+
+    </footer>
+  )
+}
